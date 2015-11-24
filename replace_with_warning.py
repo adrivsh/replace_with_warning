e0f0c4e84a14fe8354eb5ed28e76ddfae0d7c659
adrivsh <adrien.vogt.schilb@gmail.com>
2015-11-24 18:58:28 -0500

    v0.1

---
+++ replace_with_warning.py
@@ -0,0 +1,20 @@
+def replace_with_warning(series_in,dico,ignore_case=True):
+        
+    # preprocessing    
+    series_to_use = series_in
+    dico_to_use= dico
+     
+    if ignore_case:
+        series_to_use =series_to_use.str.lower()
+        dico_to_use.index=dico_to_use.index.str.lower()
+     
+    #processing    
+    out=series_to_use.replace(dico_to_use)
+    
+    # post processing
+    are_missing = ~out.isin(dico_to_use)
+    
+    if are_missing.sum()>0:
+        warnings.warn("\nbad country names : "+",".join(series_in[are_missing]))
+
+    return out
