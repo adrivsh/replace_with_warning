410d2eb808e15666fa4171989fbc4ce94a3c3cf0
adrivsh <adrien.vogt.schilb@gmail.com>
2016-11-30 16:54:46 -0500

    copy inputs to pass value not ref

--- replace_with_warning.py
+++ replace_with_warning.py
@@ -4,13 +4,14 @@ import warnings
 def replace_with_warning(series_in,dico,ignore_case=True,sep="; "):
         
     # preprocessing    
-    series_to_use = series_in
-    dico_to_use= dico
+    series_to_use = series_in.copy()
+    dico_to_use= dico.copy()
     
     if ignore_case:
         series_to_use =series_to_use.str.lower()
         dico_to_use.index=dico_to_use.index.str.lower()
-     
+        
+    
     #processing    
     out=series_to_use.replace(dico_to_use)
     
